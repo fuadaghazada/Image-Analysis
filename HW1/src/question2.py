b494c760e10710268fa56b169d0e8dfabb4a44bc
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-03-07 17:56:19 +0300

    Question 2 improved

--- HW1/src/question2.py
+++ HW1/src/question2.py
@@ -13,13 +13,13 @@ def question2_test():
 
     src_img = load_img_2D('sonnet.png')
 
-    thr_img = adaptive_threshold(src_img, (7, 7), 7, 0)
+    thr_img = adaptive_threshold(src_img, (5, 5), 5, 0)
 
-    strc_el = np.ones((3, 1)).astype(np.uint8) * 255
-
+    strc_el = np.ones((1, 3)).astype(np.uint8) * 255
     out_img = erosion(thr_img, strc_el)
 
-    # out_img = dilation(out_img, strc_el2)
+    strc_el = np.ones((1, 1)).astype(np.uint8) * 255
+    out_img = dilation(thr_img, strc_el)
 
     # Output
     save_img_from_array(out_img, out_name = 'sonnet_out.png')
