fbb00d1dd1815f22faff91ec767458fbf4266bf9
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-05-07 19:28:18 +0300

    HW3 Finalize

--- HW3/src/main.py
+++ HW3/src/main.py
@@ -23,9 +23,9 @@ images, _ = load_images()
 image = images[1]
 
 # ------- Parameters --------
-num_of_super_pixels = 300
-num_of_clusters = 2
-compactness = 5
+num_of_super_pixels = 200
+num_of_clusters = 3
+compactness = 10
 wavelengths = [2, 4, 6, 8]
 orientations = [30, 50, 70, 90]
 # ---------------------------
@@ -33,7 +33,18 @@ orientations = [30, 50, 70, 90]
 # For keeping labels of each image in dataset
 images_labels = []
 
+print("-- Parameters --")
+print("\tNumber of Super pixels:", num_of_super_pixels)
+print("\tCompactness:", compactness)
+print("\tWavelengths (for Gabor filtering):", *wavelengths)
+print("\tOrientations (for Gabor filtering):", *orientations)
+print("\tNumber of clusters:", num_of_clusters)
+print("\n********************* \n")
+
 # -- Part 1 --
+
+print("--- PART 1 ---")
+
 for i, image in enumerate(images):
 	# Segmenting
 	labels = slic(image, num_of_super_pixels, compactness)
@@ -42,17 +53,26 @@ for i, image in enumerate(images):
 	# Keeping labels
 	images_labels.append(labels)
 
+	print(f"\tSLIC algorithm is applied for image {i}")
+
 	# Saving results
 	plt.imsave('../output/slic/segmented' + str(i) + '.png', segmented_image)
+
+print("\n********************* \n")
 
 # For keeping all average Gabors
 all_data = None
 super_pixel_count = []
 
+print("--- PART 2 ---")
+
 # -- Part 2 --
+
 for i, image in enumerate(images):
 	# Gabor features and average gabors
 	result_images, average_gabors = gabor_feature_extractions(image, labels, wavelengths, orientations)
+
+	print(f"\tGabor filter extraction for image {i}")
 
 	# Saving Gabor results
 	for img in result_images:
@@ -67,18 +87,22 @@ for i, image in enumerate(images):
 	# Keeping super pixel counts
 	super_pixel_count.append(average_gabors.shape[0])
 
+print("\n********************* \n")
+
+print("--- PART 3 ---")
 
 # -- Part 3 --
 
 # Applying the K-Means
 clusters = apply_k_means(np.float32(all_data), num_of_clusters, images, images_labels, super_pixel_count)
 
+print(f"\tApplying K-means...")
+
 # Saving the clusters
 for i, cluster in enumerate(clusters):
-	overlay_img = label2rgb(cluster, images[i], image_alpha=0)
-	plt.imsave('../output/cluster/cluster' + str(i) + '.png', overlay_img)
+	false_color_img = label2rgb(cluster, images[i], image_alpha=0)
+	plt.imsave('../output/cluster/cluster' + str(i) + '.png', false_color_img)
 
-
-
+print("\n***** Processes finished! *****\n")
 
 
